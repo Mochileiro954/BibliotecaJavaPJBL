add8a75943d8eee7bba9305df72e6dfd4439aa7c
Pedro Lucas Gomes Vitorino de Oliveira <pedro.vitorino@pucpr.edu.br>
2025-05-13 21:55:43 -0300

    criei a pasta e o arquivo java do projeto, e dentro do codigo já esta criado uma classe abstrata Pessoa com seus atributos e construtor

---
+++ Proj/Main.java
@@ -0,0 +1,22 @@
+package Proj;
+
+
+abstract class  Pessoa{
+    private String nome;
+    private int cpf;
+    private int idade;
+    private String sexo;
+
+    public Pessoa(String nome,int cpf, int idade, String sexo){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
