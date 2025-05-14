dfe94d43f9bbe452928cd24ad7a45b81480ec2e1
Pedro Lucas Gomes Vitorino de Oliveira <pedro.vitorino@pucpr.edu.br>
2025-05-13 22:26:54 -0300

    adicionei a classe Cliente e fiz um teste criando um cliente  printando ele

--- Proj/Main.java
+++ Proj/Main.java
@@ -1,22 +1,77 @@
 package Proj;
 
+import java.time.LocalDate;
 
 abstract class  Pessoa{
     private String nome;
     private int cpf;
     private int idade;
     private String sexo;
+    private String email;
+    private String senha;
 
-    public Pessoa(String nome,int cpf, int idade, String sexo){
+    public Pessoa(String nome,int cpf, int idade, String sexo, String email, String senha){
         this.nome = nome;
         this.cpf = cpf;
         this.idade = idade;
         this.sexo = sexo;
+        this.email = email;
+        this.senha = senha;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public String getSexo() {
+        return sexo;
     }
 }
 
+class Cliente extends Pessoa {
+    private double saldo;
+    private LocalDate criacao;
+    
+    public Cliente(String nome,int cpf, int idade, String sexo, String email, String senha, double saldo){
+        super(nome,cpf,idade,sexo,email,senha);
+        this.saldo = saldo;
+        this.criacao = LocalDate.now();
+    }
+
+    public LocalDate getCriacao() {
+        return criacao;
+
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    @Override
+    public String toString() {
+        return "Cliente{" +
+            "nome='" + getNome() + '\'' +
+            ", cpf=" + getCpf() +
+            ", idade=" + getIdade() +
+            ", sexo='" + getSexo() + '\'' +
+            ", email='" + getEmail() + '\'' +
+            ", saldo=" + saldo +
+            ", criacao=" + criacao +
+            '}';
+}
+}
 public class Main{
     public static void main(String[] args) {
-        
+        Cliente c = new Cliente("pedro", 123, 18, "Masculino", "Pedro78446@", "oiteste", 1500.60);
+
+        System.out.println(c);
     }
 }
